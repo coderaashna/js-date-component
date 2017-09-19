986035121095ec6cb8495daaad64fbce54ce086c
coderaashna <aashna.iitk@gmail.com>
2017-09-20 00:13:42 +0530

    1. Formatted the code 2. Array indexing

--- app.js
+++ app.js
@@ -1,79 +1,92 @@
+function validate() {
+    var Date = [];
+    Date.push({
+        Month: 0,
+        Days: 31
+    }, {
+        Month: 1,
+        Days: 29
+    }, {
+        Month: 2,
+        Days: 31
+    }, {
+        Month: 3,
+        Days: 30
+    }, {
+        Month: 4,
+        Days: 31
+    }, {
+        Month: 5,
+        Days: 30
+    }, {
+        Month: 6,
+        Days: 31
+    }, {
+        Month: 7,
+        Days: 31
+    }, {
+        Month: 8,
+        Days: 30
+    }, {
+        Month: 9,
+        Days: 31
+    }, {
+        Month: 10,
+        Days: 30
+    }, {
+        Month: 11,
+        Days: 31
+    });
+    //Date.push({id:200,name:'Mahesh',age:35});
 
-function validate()
-{
-var Date = [];
-Date.push({Month:1,Days:31},{Month:2,Days:29},{Month:3,Days:31},{Month:4,Days:30},{Month:5,Days:31},{Month:6,Days:30},{Month:7,Days:31},{Month:8,Days:31},{Month:9,Days:30},{Month:10,Days:31},{Month:11,Days:30},{Month:12,Days:31});
-//Date.push({id:200,name:'Mahesh',age:35});
+    if (document.getElementsByName('day')[0].value > 31 || document.getElementsByName(('month')[0] + 1).value > 12 || document.getElementsByName('year')[0].value.length > 4) {
+        alert("Invalid Date");
+        return;
+    }
 
-if (document.getElementsByName('day')[0].value >31 || document.getElementsByName('month')[0].value >12 || document.getElementsByName('year')[0].value.length >4)
-{
-alert("Invalid Date");
-return;
-}
+    var m = document.getElementsByName('month')[0].value;
 
-var m=document.getElementsByName('month')[0].value;
+    if (document.getElementsByName('day')[0].value > Date[m].Days) {
+        alert("Invalid Date");
+        return;
+    }
+    if (document.getElementsByName('month')[0].value == 1) {
 
-if (document.getElementsByName('day')[0].value>Date[m].Days)
-{
-alert("Invalid Date");
-return;
-}
-if(document.getElementsByName('month')[0].value==2)
-{
+        if (document.getElementsByName('year')[0].value % 100 == 0) {
 
-if(document.getElementsByName('year')[0].value%100==0)
-{
+            if (document.getElementsByName('year')[0].value % 400 == 0) {
+                if (document.getElementsByName('day')[0].value > 29) {
+                    alert("Invalid Date");
+                    return;
+                } else
+                    alert("Valid Date");
 
-if(document.getElementsByName('year')[0].value%400==0)
-{
-if(document.getElementsByName('day')[0].value>29)
-{
-alert("Invalid Date");
-return;
-}
-else
-alert("Valid Date");
+            } else {
+                if (document.getElementsByName('day')[0].value > 28) {
+                    alert("Invalid Date");
+                    return;
+                } else
+                    alert("Valid Date");
+            }
+        } else {
+            if (document.getElementsByName('year')[0].value % 4 == 0) {
 
-}
-else
-{
-if(document.getElementsByName('day')[0].value>28)
-{
-alert("Invalid Date");
-return;
-}
-else
-alert("Valid Date");
-}
-}
+                if (document.getElementsByName('day')[0].value > 29) {
+                    alert("Invalid Date");
+                    return;
+                } else
+                    alert("Valid Date");
+            } else {
+                if (document.getElementsByName('day')[0].value > 28) {
+                    alert("Invalid Date");
+                    return;
+                } else
+                    alert("Valid Date");
 
-else
-{
-if(document.getElementsByName('year')[0].value%4==0)
-{
+            }
 
-if(document.getElementsByName('day')[0].value>29)
-{
-alert("Invalid Date");
-return;
-}
-else
-alert("Valid Date");
-}
-else
-{
-if(document.getElementsByName('day')[0].value>28)
-{
-alert("Invalid Date");
-return;
-}
-else
-alert("Valid Date");
+        }
 
-}
-
-}
-
-}
-alert("Valid date");
+    }
+    alert("Valid date");
 }
